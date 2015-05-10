8a082a068839746ecffa53037c3a27f406eb4bc8
Jeppe Hartmund <jeppe.hartmund@gmail.com>
2015-05-10 21:41:35 +0200

    minor changes

--- CRLogic.pl
+++ CRLogic.pl
@@ -2,6 +2,8 @@
 
 :-module(CRLogic,[ae/3,ee/3,isa/2]).
 use_module('pancreas').
+set_prolog_flags(unknown,fail).
+
 % By definition...
 ae(C,identity,D) :- isa(C,D).
 
@@ -10,14 +12,14 @@ ae(C_,R,D) :- isa(C_,C), ae_def(C,R,D).
 ae(C,R,D_) :- isa(D,D_), ae_def(C,R,D).
 %ae_(C,R,D) :- aa_(C,R,D).
 
-ee(C,R,D) :- isa(C_,C), ee_2(C_,R,D).
-ee_2(C,R,D) :- ee_def(C,R,D).
-ee_2(C,R,D) :- ae_(C,R,D).
+ee(C,R,D) :- isa(C_,C), ee_(C_,R,D).
+ee_(C,R,D) :- ee_def(C,R,D).
+ee_(C,R,D) :- ae(C,R,D).
 %% Inference rules for aa
-%aa_(C_,R,D) :- isa_(C_,C), aa(C,R,D).
-%aa_(C,R,D_) :- isa_(D_,D), aa(C,R,D).
 
-%ea(C,R,D) :- aa_(C,R,D).
+aa(C_,R,D) :- isa_(C_,C), aa_def(C,R,D).
+aa(C,R,D_) :- isa_(D_,D), aa_def(C,R,D).
+ea(C,R,D) :- aa(C,R,D).
 
 % Properties of isa...
 isa(X,Y) :- tisa(X,Y,[]).
